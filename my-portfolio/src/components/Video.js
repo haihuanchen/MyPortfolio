8138876b037b81bb10f273e40a4d44fefab09306
haihuanchen <haihuan.chen@hotmail.com>
2020-12-03 21:41:50 -0500

    loop videos for homepage

--- my-portfolio/src/components/Video.js
+++ my-portfolio/src/components/Video.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from './Button';
 import './Video.css';
 import '../App.css';
 
 function Video() {
+    const urls = ["/videos/video-1.mp4", "/videos/video-2.mp4"];
+    const [currentVideo, setCurrentVideo] = useState(0);
+
+    const handleEnded = () => {
+        if (currentVideo < urls.length - 1){
+            setCurrentVideo(currentVideo => currentVideo + 1)
+        }else{
+            setCurrentVideo(0)
+        }
+    }
+
     return (
         <div className='video-container'>
-            <video src="/videos/video-1.mp4" autoPlay loop muted />
+            <video src={urls[currentVideo]} autoPlay onEnded={handleEnded} muted />
             <h1>Coding Journey Awaits</h1>
             <p>What are you waiting for?</p>
             <div className="video-btns">
